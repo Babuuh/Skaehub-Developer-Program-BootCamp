cee60450cc6a50991ae81ea8b1521c990a01119c
Kihara Njoroge <46190291+Babuuh@users.noreply.github.com>
2021-06-23 13:02:15 +0300

    question 6

---
+++ day 2/simple_statistics.py
@@ -0,0 +1,10 @@
+from statistics import *
+
+mylist = [43,95,690,293,3,77,301,509,111,866]
+
+print('Mean:',format(mean(mylist)))
+print('Meadian:', format(median(mylist)))
+print('Mode:', format(mode(mylist)))
+print('Variance:', format(variance(mylist)))
+print('Std Deviation:', format(stdev(mylist)))
+print('Harmonic Mean:',format(harmonic_mean(mylist)))
